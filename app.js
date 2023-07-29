40a99e316a6b33cf2bea5cca8db41f69b829cb5e
Jetro Kanerva <125794469+jetkanerva@users.noreply.github.com>
2023-07-29 17:55:17 +0300

    edit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import { Application } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { router } from "./routes/routes.js";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import { renderMiddleware } from "./middlewares/renderMiddleware.js";
+import { serve } from "./deps.js";
 
 const app = new Application();
 
@@ -10,10 +11,6 @@ app.use(renderMiddleware);
 
 app.use(router.routes());
 
-if (!Deno.env.get("TEST_ENVIRONMENT")) {
-    app.listen({ port: 7777 });
-}
-
-export default app ;
+serve(app, { port: 7777 });
 
 
