b44f9425c33b1d47b014e6eb0e70e01802e11a89
Jetro Kanerva <125794469+jetkanerva@users.noreply.github.com>
2023-07-29 18:12:31 +0300

    update app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-import { Application } from "https://deno.land/x/oak@v11.1.0/mod.ts";
-import { router } from "./routes/routes.js";
-import { errorMiddleware } from "./middlewares/errorMiddleware.js";
-import { renderMiddleware } from "./middlewares/renderMiddleware.js";
+import {Application} from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import {router} from "./routes/routes.js";
+import {errorMiddleware} from "./middlewares/errorMiddleware.js";
+import {renderMiddleware} from "./middlewares/renderMiddleware.js";
+import { serve } from "./deps.js";
 
 const app = new Application();
 
@@ -10,6 +11,11 @@ app.use(renderMiddleware);
 
 app.use(router.routes());
 
-app.listen({ port: 7777 });
+
+const handleRequest = async (request) => {
+    return await app.handle(request);
+};
+
+serve(handleRequest, { port: 7777 });
 
 
