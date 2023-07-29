b8e3020e90c6341fa16579494d352b0a65874d35
Jetro Kanerva <125794469+jetkanerva@users.noreply.github.com>
2023-07-29 18:07:34 +0300

    edit

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import { Application } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { router } from "./routes/routes.js";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import { renderMiddleware } from "./middlewares/renderMiddleware.js";
-import { serve } from "./deps.js";
 
 const app = new Application();
 
@@ -11,6 +10,6 @@ app.use(renderMiddleware);
 
 app.use(router.routes());
 
-serve(app, { port: 7777 });
+app.listen({ port: 7777 });
 
 
