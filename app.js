de1b15e57800ecce39b8f675219ce0665dae9282
Jetro Kanerva <125794469+jetkanerva@users.noreply.github.com>
2023-07-29 20:49:53 +0300

    delete middleware

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 import {Application} from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import {router} from "./routes/routes.js";
-import {errorMiddleware} from "./middlewares/errorMiddleware.js";
-import {renderMiddleware} from "./middlewares/renderMiddleware.js";
 import { serve } from "./deps.js";
 
 const app = new Application();
-
-app.use(errorMiddleware);
-app.use(renderMiddleware);
-
 app.use(router.routes());
-
 
 const handleRequest = async (request) => {
     return await app.handle(request);
