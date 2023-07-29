7b28a92ad2af573e82847ec4c970b915021c17fd
Jetro Kanerva <125794469+jetkanerva@users.noreply.github.com>
2023-07-29 21:07:08 +0300

    added middlewares

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-import {Application} from "https://deno.land/x/oak@v11.1.0/mod.ts";
-import {router} from "./routes/routes.js";
+import { Application } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import { router } from "./routes/routes.js";
 import { serve } from "./deps.js";
+import {renderMiddleware} from "./middlewares/renderMiddleware.js";
 
 const app = new Application();
+app.use(renderMiddleware);
+
 app.use(router.routes());
 
 const handleRequest = async (request) => {
