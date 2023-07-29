70401c7753ea628164bb872fa0ed4d713841aac0
Jetro Kanerva <125794469+jetkanerva@users.noreply.github.com>
2023-07-29 21:25:15 +0300

    update

--- deps.js
+++ deps.js
@@ -1,4 +1,5 @@
 export { serve } from "https://deno.land/std@0.171.0/http/server.ts";
+export { Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 export { configure, renderFile } from "https://deno.land/x/eta@v2.0.0/mod.ts";
 import postgres from "https://deno.land/x/postgresjs@v3.3.3/mod.js";
 export { postgres };
